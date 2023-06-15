fc8356d2d2a8e81b419c58a9d9eb277b2bad7eaa
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-06-15 15:22:15 +0200

    Evaluation ini, Training measured with confusion matrix

---
+++ evaluation.py
@@ -0,0 +1,7 @@
+from model import model, scaler
+
+# Make a prediction
+new_value = [[0.19499106, 0.32795699, 0.38976378, 1, 0, 1, 0, 0,
+              0]]  # enter new data in 2D array. Only numbers + dummy variables.
+new_value = scaler.transform(new_value)  # Don't forget to scale!
+model.predict(new_value)
--- training.py
+++ training.py
@@ -1,9 +1,10 @@
-from model import model, y_train, X_train
+from model import model, y_train, X_train, X_test, y_test
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.python.keras import Sequential
@@ -17,3 +18,22 @@ model.fit(X_train, y_train, epochs=100, verbose=1)
 # Loss vs epochs
 loss = model.history.history['loss']
 sns.lineplot(x=range(len(loss)), y=loss)
+
+# Model accuracy
+model.evaluate(X_test, y_test, verbose=1)
+
+# Confusion Matrix
+y_pred = model.predict(X_test)
+y_pred = y_pred.argmax(axis=1)
+y_test = y_test.argmax(axis=1)
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+# Visualize confusion matrix
+plt.figure(figsize=(10, 7))
+sns.heatmap(cm, annot=True, fmt='d')
+plt.xlabel('Predicted')
+plt.ylabel('Truth')
+plt.show()
+
+model.save('my_model.h5')
