4a935f05deed02bd659451d3040fa78d0303bc25
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-06-15 14:52:04 +0200

    Convert text to categorical/numpy array, one hot encoding use. Data scaled with minMax

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from pickle import dump, load
 
 # Read Data
 dataFrame = pd.read_csv('seattle-weather.csv')
@@ -14,8 +16,24 @@ dataFrame = dataFrame.set_index(['date'])
 print(dataFrame)
 
 # Assign X and y Dataframe
-pd.set_option('display.max_columns', None) # print all columns
-X = dataFrame.iloc[ : , 0:5] # all rows and cols 0-4
-y = dataFrame.iloc[ : , -1] # all rows and last col
+pd.set_option('display.max_columns', None)  # print all columns
+X = dataFrame.iloc[:, 0:5]  # all rows and cols 0-4
+y = dataFrame.iloc[:, -1]  # all rows and last col
 print(X.head())
 print(y.head())
+
+#  Convert text to categorical data and to numpy array
+X = pd.get_dummies(X)
+y = pd.get_dummies(y)  # one hot encoding categorical values
+X = X.values  # convert from pandas to numpy array
+y = y.values
+print("Converted to Categorical X\n", X)
+print("Converted to Categorical y\n", y)
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+X = scaler.fit_transform(X)
+
+#  Scaler
+dump(scaler, open('scaler.pkl', 'wb'))  # save the scaler
+print("Scaled X\n", X)
+print("Scaled y\n", y)
