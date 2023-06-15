855ac34b66000e033fe232ede68e5b134d304379
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-06-15 15:27:39 +0200

    Clean-up

--- evaluation.py
+++ evaluation.py
@@ -4,4 +4,4 @@ from model import model, scaler
 new_value = [[0.19499106, 0.32795699, 0.38976378, 1, 0, 1, 0, 0,
               0]]  # enter new data in 2D array. Only numbers + dummy variables.
 new_value = scaler.transform(new_value)  # Don't forget to scale!
-model.predict(new_value)
+print(model.predict(new_value))
--- training.py
+++ training.py
@@ -1,19 +1,10 @@
 from model import model, y_train, X_train, X_test, y_test
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.optimizer_v2.adam import Adam
-from pickle import dump, load
+from sklearn.metrics import confusion_matrix
 
 # Train
-model.fit(X_train, y_train, epochs=100, verbose=1)
+model.fit(X_train, y_train, epochs=1000, verbose=1)
 
 # Loss vs epochs
 loss = model.history.history['loss']
